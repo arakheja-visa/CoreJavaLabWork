30a9ecd88c19e9570e956774792b98b73bd0f8ac
arakheja-visa <anuj.00091@gmail.com>
2016-09-12 14:06:39 -0600

    Day 1 Excersize
    Temperature Converter F to C

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author arakheja
+ */
+public class Main { 
+    
+ double f =0;
+    public static void main(String[] args) {
+        System.out.println("Please enter the temperature in Fahrenhite..");
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        double f = Double.valueOf(input);
+       double c = 5 * (f-32)/9;
+        System.err.println("Value in Celsius: " +c); 
+    }
+}
