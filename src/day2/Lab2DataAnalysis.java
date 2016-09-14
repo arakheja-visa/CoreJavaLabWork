a8e6084a44806b1d6e5e2dff53228a8353e0d251
arakheja-visa <anuj.00091@gmail.com>
2016-09-14 15:14:22 -0600

    Day 2 Lab 2 Data Analysis
    Day 2 Lab 2 Added

---
+++ src/day2/Lab2DataAnalysis.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package day2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author arakheja
+ */
+public class Lab2DataAnalysis {
+    
+    public static void main(String[] args) {
+        
+        System.out.print("Please enter the number of items you want to provide: ");
+        Scanner sc = new Scanner(System.in);
+        int sizeOfArray = sc.nextInt();
+        int[] array = new int[sizeOfArray];
+         
+        for(int i = 0; i<array.length; i++){
+            System.out.println("Please enter the " +(i+1)+ " number" );   
+            array[i]=sc.nextInt();
+        }
+        int min = array[0];
+        int max = array[0];
+        int sum =0;
+        for(int i = 0; i<array.length; i++){
+            if(min > array[i] ){
+            min = array[i];
+            }
+            if(max < array[i] ){
+            max = array[i];
+            }
+            sum = sum + array[i];
+            
+        }
+        System.out.println ("Smallest number added: " +min + ", Largest number added: " + max + " and Mean : " + (sum/sizeOfArray));
+    }
+        
+    
+}
